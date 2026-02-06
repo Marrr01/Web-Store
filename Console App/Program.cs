b5c4c5513817104e5f0a47ce0d5110211ab85f97
Кирилл <win10@PC>
2026-02-06 10:06:32 +0200

    test1

--- Console App/Program.cs
+++ Console App/Program.cs
@@ -7,6 +7,8 @@ namespace Console_App
     {
         static void Main(string[] args)
         {
+            var t1 = 1;
+
             #region test
             using (ApplicationContext db = new ApplicationContext())
             {
